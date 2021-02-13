05faaff30b0f93d6424e9c20bb671c2f900691a7
Dave <dave95.b@gmail.com>
2021-02-13 11:42:04 +0100

    Add basic scripts

---
+++ Assets/Scripts/OnRenderImageDispatcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Utils
+{
+    public class OnRenderImageDispatcher : MonoBehaviour
+    {
+        public event Action<RenderTexture> OnImageRendered;
+
+        private void OnRenderImage(RenderTexture src, RenderTexture dest)
+        {
+            OnImageRendered?.Invoke(dest);
+        }
+
+        private void OnDestroy()
+        {
+            OnImageRendered = null;
+        }
+    }
+}
